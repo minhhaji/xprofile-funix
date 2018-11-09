8350626197c57ec4af03897f476946d7a5a933a1
Minh <minhkyuuby@gmail.com>
2018-11-09 23:17:49 +0700

    'demo3'

--- src/components/form.js
+++ src/components/form.js
@@ -1,12 +1,10 @@
 import React, {Component} from 'react';
-import {Card, CardHeader, CardBody, Label, Row, Col, Button} from 'reactstrap';
-import { Control, Form, Errors, Field } from 'react-redux-form';
+import {Card, CardHeader, CardBody, Label, Col, Button} from 'reactstrap';
+import { Control, Form, Errors} from 'react-redux-form';
 
 const required = (val) => val && val.length;
-const maxLength = (len) => (val) => !(val) || (val.length <= len);
 const minLength = (len) => (val) => val && (val.length >= len);
 const isNumber = (val) => !isNaN(Number(val));
-const validEmail = (val) => /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(val);
 
 class Background extends Component {
     constructor(props) {
@@ -35,7 +33,7 @@ class Background extends Component {
             <p>Any files that are upleaded will be shared outside of the organization they belong to.</p>
             <p className='text-danger'>* required</p>
             <Form model='form' onSubmit={(values) => this.handleSubmit(values)}>
-            <div id='ttcn'><h3>I. Thông tin cá nhân</h3>
+            <div id='ttcn'><h3>I. THÔNG TIN CÁ NHÂN</h3>
                 
                 <div className="form-group">
                         <Label htmlFor="name" md={12}><h4>1. Họ và tên: <span className='text-danger'>*</span></h4></Label>
@@ -178,9 +176,9 @@ class Background extends Component {
                         </Col>
                 </div>
                 <div className="form-group">
-                        <Label htmlFor="passport" md={12}><h4>8. Số chứng minh nhân dân/ hộ chiếu: <span className='text-danger'>*</span></h4><small>Chú ý: viết đúng các chữ số từ 0-9</small></Label>
+                        <Label htmlFor="passportNum" md={12}><h4>8. Số chứng minh nhân dân/ hộ chiếu: <span className='text-danger'>*</span></h4><small>Chú ý: viết đúng các chữ số từ 0-9</small></Label>
                         <Col md={12}>
-                            <Control.text model=".passport" id="passport" name="passport"
+                            <Control.text model=".passportNum" id="passportNum" name="passportNum"
                                 placeholder="Your Answer"
                                 className="form-control"
                                 validators={{
@@ -189,10 +187,10 @@ class Background extends Component {
                                     />
                             <Errors
                                 className="text-danger"
-                                model=".passport"
+                                model=".passportNum"
                                 show="touched"
                                 messages={{
-                                    required: 'Required! ', isNumber: 'must be digits form 0 tp 9'
+                                    required: 'Required! ', isNumber: 'must be digits form 0 to 9'
                                 }}
                                 />
                         </Col>
@@ -258,53 +256,128 @@ class Background extends Component {
                         </Col>
                 </div>
                 <div className="form-group">
-                        <Label htmlFor="job" md={12}><h4>12. Nghề nghiệp: <span className='text-danger'>*</span></h4></Label>
+                        <Label htmlFor="job" md={12}><h4>12. Nghề nghiệp: </h4></Label>
                         <Col md={12}>
                             <Control.text model=".job" id="job" name="job"
                                 placeholder="Your Answer"
                                 className="form-control"
-                                validators={{
-                                    required
-                                }}
                                     />
-                            <Errors
-                                className="text-danger"
-                                model=".job"
-                                show="touched"
-                                messages={{
-                                    required: 'Required! ',
-                                }}
-                                />
                         </Col>
                 </div></div>
-                <h3>II. Thông tin liên hệ khác</h3>
-                <p>Trong trường hợp không thể liên hệ với anh/chị qua số điện thoại di động cá nhân, nhà trường có thể liên hệ với ai? Anh chị vui lòng cung cấp số điện thoại, tên, mối quan hệ,... ví dụ: Nguyễn Văn A-Bố-0</p>
+                <div id='ttlhk'><h3>II. THÔNG TIN LIÊN HỆ KHÁC</h3>
+                <Col><p>Trong trường hợp không thể liên hệ với anh/chị qua số điện thoại di động cá nhân, nhà trường có thể liên hệ với ai? Anh chị vui lòng cung cấp số điện thoại, tên, mối quan hệ,... ví dụ: Nguyễn Văn A-Bố-0</p></Col>
                 <div className="form-group">
-                        <Label htmlFor="nameRelative" md={12}><h4>1. Họ và tên: <span className='text-danger'>*</span></h4></Label>
+                        <Label htmlFor="nameRelative" md={12}><h4>1. Họ và tên: </h4></Label>
                         <Col md={12}>
                             <Control.text model=".nameRelative" id="nameRelative" name="nameRelative"
                                 placeholder="Your Answer"
                                 className="form-control"
+                                    />
+                        </Col>
+                </div>
+                <div className="form-group">
+                        <Label htmlFor="relationship" md={12}><h4>2. Mối quan hệ: </h4></Label>
+                        <Col md={12}>
+                            <Control.text model=".relationship" id="relationship" name="relationship"
+                                placeholder="Your Answer"
+                                className="form-control"
+                                    />
+                        </Col>
+                </div>
+                <div className="form-group">
+                        <Label htmlFor="phonenum" md={12}><h4>3. Số điện thoại: </h4><small>Chú ý: viết liền các chữ số từ 0-9, không có kí tự đặc biệt. Định dạng: Mã nướcxxxxxxxx. Ví dụ: Việt Nam: +84xxxxxxxx</small></Label>
+                        <Col md={12}>
+                            <Control.text model=".phonenum" id="phonenum" name="phonenum"
+                                placeholder="Your Answer"
+                                className="form-control"
                                 validators={{
-                                    required, minLength: minLength(3),
+                                    isNumber
                                 }}
                                     />
                             <Errors
                                 className="text-danger"
-                                model=".nameRelative"
+                                model=".phonenum"
+                                show="touched"
+                                messages={{
+                                    isNumber: 'must be digits form 0 to 9'
+                                }}
+                                />
+                        </Col>
+                </div></div>
+                <div id='ldtg'><h3>III. LÝ DO THAM GIA HỌC TẠI FUNIX</h3>
+                <Col><p>Anh/chị vui lòng thể hiện chi tiết về nhu cầu, kế hoạch, mục tiêu, động cơ học tập của mình</p></Col>
+                <div className="form-group">
+                        <Label htmlFor="reason" md={12}><h4>1. Tại sao bạn quyết định học Công nghệ thông tin và lựa chọn FUNIX <span className='text-danger'>*</span></h4><small>Anh/chị vui lòng chia sẻ thông tin khoảng 5-10 dòng</small></Label>
+                        <Col md={12}>
+                            <Control.textarea model=".reason" id="reason" name="reason"
+                                placeholder="Your Answer"
+                                className="form-control"
+                                rows='5'
+                                validators={{
+                                    required, minLength: minLength(150),
+                                }}
+                                    />
+                            <Errors
+                                className="text-danger"
+                                model=".reason"
                                 show="touched"
                                 messages={{
                                     required: 'Required! ',
-                                    minLength: 'Must be greater than 2 characters',
+                                    minLength: 'Must be 150 or more',
                                 }}
                                 />
                         </Col>
                 </div>
                 <div className="form-group">
-                        <Label htmlFor="relationship" md={12}><h4>2. Mối quan hệ: <span className='text-danger'>*</span></h4></Label>
+                        <Label htmlFor="target" md={12}><h4>2. Mục tiêu của bạn sau khi học tại FUNIX <span className='text-danger'>*</span></h4><small>Anh/chị vui lòng chia sẻ thông tin khoảng 5-10 dòng</small></Label>
                         <Col md={12}>
-                            <Control.text model=".relationship" id="relationship" name="relationship"
+                            <Control.textarea model=".target" id="target" name="target"
                                 placeholder="Your Answer"
+                                className="form-control"
+                                rows='5'
+                                validators={{
+                                    required, minLength: minLength(150),
+                                }}
+                                    />
+                            <Errors
+                                className="text-danger"
+                                model=".target"
+                                show="touched"
+                                messages={{
+                                    required: 'Required! ',
+                                    minLength: 'Must be greater than 150 characters',
+                                }}
+                                />
+                        </Col>
+                </div>
+                <div className="form-group">
+                        <Label htmlFor="opinion" md={12}><h4>3. Ý kiến của bạn muốn chia sẽ với FUNIX <span className='text-danger'>*</span></h4><small>Anh/chị vui lòng chia sẻ mong muốn, nguyện vọng hoặc các câu hỏi dành cho FUNIX, khoảng 5-10 dòng</small></Label>
+                        <Col md={12}>
+                            <Control.textarea model=".opinion" id="opinion" name="opinion"
+                                placeholder="Your Answer"
+                                className="form-control"
+                                rows='5'
+                                validators={{
+                                    required, minLength: minLength(150),
+                                }}
+                                    />
+                            <Errors
+                                className="text-danger"
+                                model=".opinion"
+                                show="touched"
+                                messages={{
+                                    required: 'Required! ',
+                                    minLength: 'Must be greater than 150 characters',
+                                }}
+                                />
+                        </Col>
+                </div></div>
+                <div id='cpt'><h3>IV. HOÀN THIỆN HỒ SƠ</h3>
+                <Col><p>Anh/chị vui lòng hoàn thiện giấy tờ để nhà trường lập hồ sơ sinh viên của anh/chị</p></Col>
+                <div className="form-group">
+                        <Label md={12}><h4>1. Ảnh 3X4 <span className='text-danger'>*</span></h4><small>Anh/chị vui lòng scan rõ nét, chụp trên nền trắng hoặc xanh dương, tên file ảnh là tên đầy đủ của sinh viên</small></Label>
+                        <Col md={12}>
+                            <Control.file model=".photo" id="photo" name="photo"
                                 className="form-control"
                                 validators={{
                                     required
@@ -312,7 +385,7 @@ class Background extends Component {
                                     />
                             <Errors
                                 className="text-danger"
-                                model=".relationship"
+                                model=".photo"
                                 show="touched"
                                 messages={{
                                     required: 'Required! '
@@ -321,24 +394,38 @@ class Background extends Component {
                         </Col>
                 </div>
                 <div className="form-group">
-                        <Label htmlFor="phonenum" md={12}><h4>3. Số điện thoại: <span className='text-danger'>*</span></h4><small>Chú ý: viết liền các chữ số từ 0-9, không có kí tự đặc biệt.</small></Label>
+                        <Label md={12}><h4>2. CMND/ Hộ chiếu/ Giấy khai sinh<span className='text-danger'>*</span></h4><small>1 bản scan/chụp ảnh CMND 2 mặt hoặc chộ chiếu. Với các sinh viên chưa đủ tuổi làm CMND vui lòng gửi giấy khai sinh.</small></Label>
                         <Col md={12}>
-                            <Control.text model=".phonenum" id="phonenum" name="phonenum"
-                                placeholder="Your Answer"
+                            <Control.file model=".passportPhoto" id="passportPhoto" name="passportPhoto"
                                 className="form-control"
                                 validators={{
-                                    required, isNumber
+                                    required
                                 }}
                                     />
                             <Errors
                                 className="text-danger"
-                                model=".phonenum"
+                                model=".passportPhoto"
                                 show="touched"
                                 messages={{
-                                    required: 'Required! ', isNumber: 'must be digits form 0 tp 9'
+                                    required: 'Required! ',
                                 }}
                                 />
                         </Col>
+                </div>
+                <div className="form-group">
+                        <Label md={12}><h4>3. Bằng tốt nghiệp THPT/bằng cấp cao nhất đã có </h4><small>Với anh/chị đã tốt nghiệp THPT có nhu cầu lấy bằng Đại học. Anh/chị vui lòng nộp 1 bản scan/chụp ảnh Bằng tốt nghiệp THPT hoặc bằng cấp cao nhất đã có (có thể nộp Giấy chứng nhận tốt nghiệp tạm thời và bổ sung sau khi có Bằng).</small></Label>
+                        <Col md={12}>
+                            <Control.file model=".target" id="target" name="target"
+                                className="form-control"
+                                    />
+                        </Col>
+                </div></div>
+                <div className="form-group">
+                                    <Col>
+                                        <Button type="submit" className="btn btn-primary btn-block">
+                                        Submit 
+                                        </Button>
+                                    </Col>
                 </div>
                 </Form>
                 </CardBody>
